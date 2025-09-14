cd26d98a79aa9d9d590fa0fc889e29e332ddb12f
Sahil Davkhar <sahildavkhar@gmail.com>
2025-09-14 23:23:16 +0530

    Second Commit

---
+++ SumOfNum.java
@@ -0,0 +1,12 @@
+public class SumOfNum {
+    public static void solve(int n) {
+        int sum = n * (n + 1) / 2;
+        System.out.println("The Sum of Numbers is:" + sum);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        solve(n);
+    }   
+    
+}
