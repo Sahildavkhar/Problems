7f4740e91866e0bf7082c92beba39bce746ee9e9
Sahil Davkhar <sahildavkhar@gmail.com>
2025-09-15 23:14:44 +0530

    Sixth Commit

---
+++ FrequencyHash.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class FrequencyHash {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 1, 2};
+        int n = arr.length;
+        FrequencyHash(arr, n);
+    }
+
+    public static void FrequencyHash(int arr[], int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+            else{
+                map.put(arr[i], 1);
+            }
+        }        
+        // Traverse through map and print frequencies
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println("" + entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
