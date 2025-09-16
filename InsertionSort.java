bbc327484fc34ae6ffcf2e6fc7fbb965eaee89a0
Sahil Davkhar <sahildavkhar@gmail.com>
2025-09-16 22:17:37 +0530

    Eighth Commit

---
+++ InsertionSort.java
@@ -0,0 +1,36 @@
+// Insertion Sort Algorithm
+
+// Approach: 
+
+// Select an element in each iteration from the unsorted array(using a loop).
+// Place it in its corresponding position in the sorted part and shift the remaining elements accordingly (using an inner loop and swapping).
+// The “inner while loop” basically shifts the elements using swapping.
+
+public class InsertionSort {
+    public static void InsertionSort(int[] arr, int n){
+        for(int i = 0; i <= n - 1; i++){
+            int j = i;
+            while(j > 0 && arr[j - 1] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+        System.err.println("After Insertion Sort");
+        for(int i = 0; i < n; i++){
+            System.err.print(arr[i] + " ");
+        }
+        System.err.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 3, 2, 1};
+        int n = arr.length;
+        System.err.println("Before Insertion Sort");
+        for(int i = 0; i < n; i++){
+            System.err.print(arr[i] + " ");
+        }
+        System.err.println();
+        InsertionSort(arr, n);
+    }
+}
