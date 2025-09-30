9505b92caaf20b13f1a5ebdd168be6792931dd79
Sahil Davkhar <sahildavkhar@gmail.com>
2025-09-30 13:16:54 +0530

    Kth Largest Number

---
+++ KthLargest.java
@@ -0,0 +1,26 @@
+// Find the Kth largest element in an array using a min-heap.
+// Time Complexity: O(N log K), Space Complexity: O(K)
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for(int num : nums){
+            minHeap.add(num);
+            if(minHeap.size() > k){
+                minHeap.poll();
+
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        KthLargest kl = new KthLargest();
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println(kl.findKthLargest(nums, k)); // Output: 5
+    }
+}
