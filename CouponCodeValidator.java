b33d86798a04f46ad7aba3c73e8f49b5b12a638c
Sahil Davkhar <sahildavkhar@gmail.com>
2025-12-13 22:49:46 +0530

    Coupon Code Validator

---
+++ CouponCodeValidator.java
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/coupon-code-validator/?envType=daily-question&envId=2025-12-13
+
+import java.util.*;
+
+class CouponCodeValidator{
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<String>res = new ArrayList();
+
+        List<String>e = new ArrayList();
+        List<String>g = new ArrayList();
+        List<String>p = new ArrayList();
+        List<String>r = new ArrayList();
+        
+        for(int i=0;i<isActive.length;i++){
+            if(isActive[i]){
+                if(businessLine[i].equals("electronics") 
+                    || businessLine[i].equals("grocery")
+                  || businessLine[i].equals("pharmacy")
+                  || businessLine[i].equals("restaurant")){
+                    if(code[i].length() == 0)continue;
+                    boolean found = true;
+                    for(int j=0;j<code[i].length();j++){
+                        char ch = code[i].charAt(j);
+                        if (!Character.isLetterOrDigit(ch) && ch != '_') {
+                            found = false;
+                            break;
+                        }
+                    }
+
+                    if(found){
+                        if(businessLine[i].startsWith("e"))e.add(code[i]);
+                        if(businessLine[i].startsWith("g"))g.add(code[i]);
+                        if(businessLine[i].startsWith("p"))p.add(code[i]);
+                        if(businessLine[i].startsWith("r"))r.add(code[i]);
+                    }
+                  }
+            }
+        }
+        Collections.sort(e);
+        Collections.sort(g);
+        Collections.sort(p);
+        Collections.sort(r);
+        res.addAll(e);
+        res.addAll(g);
+        res.addAll(p);
+        res.addAll(r);
+        
+        return res;
+    }
+
+    public static void main(String[] args) {
+        CouponCodeValidator validator = new CouponCodeValidator();
+        String[] codes = {"SAVE10", "GROCERY_20", "PHARMA#30", "RESTAURANT50", "ELECTRO_15"};
+        String[] businessLines = {"electronics", "grocery", "pharmacy", "restaurant", "electronics"};
+        boolean[] isActive = {true, true, false, true, true};
+
+        List<String> validCoupons = validator.validateCoupons(codes, businessLines, isActive);
+        for (String coupon : validCoupons) {
+            System.out.println(coupon);
+        }
+    }
+}
