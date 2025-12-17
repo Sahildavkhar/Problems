e2fba0306e5f15f1b240a869d44f7e37fbae952d
Sahil Davkhar <sahildavkhar@gmail.com>
2025-12-17 14:54:45 +0530

    Buy Sell Stock 2

---
+++ Python/BuySellStock.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-v/description/?envType=daily-question&envId=2025-12-17
+
+class Solution:
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+            n = len(prices)
+            dp = [[0] * 3 for _ in range(k + 1)]
+
+            for j in range(1, k + 1):
+                dp[j][1] = -prices[0]
+                dp[j][2] = prices[0]
+
+            for i in range(1, n):
+                for j in range(k, 0, -1):
+                    dp[j][0] = max(dp[j][0], max(dp[j][1] + prices[i], dp[j][2] - prices[i]))
+                    dp[j][1] = max(dp[j][1], dp[j - 1][0] - prices[i])
+                    dp[j][2] = max(dp[j][2], dp[j - 1][0] + prices[i])
+
+            return dp[k][0]
+    
+if __name__ == "__main__":
+    from typing import List
+    size = int(input())
+    prices = [int(t) for t in input().strip().split()]
+    k = int(input())
+    sol = Solution()
+    ans = sol.maximumProfit(prices, k)
+    print(ans)
